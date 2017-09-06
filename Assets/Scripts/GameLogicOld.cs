9d9e90a3d41a50e01625474ae4eb5285659e13cd
Stephanie Hawn <Stephanie.hawn@hbo.com>
2017-09-06 09:41:25 -0700

    added puzzle mechanic

---
+++ Assets/Scripts/GameLogicOld.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameLogicOld : MonoBehaviour {
+
+	public GameObject player;
+	public GameObject startUI, restartUI;
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void toggleUI() {
+		startUI.SetActive (!startUI.activeSelf);
+		restartUI.SetActive (!restartUI.activeSelf);
+	}
+}
